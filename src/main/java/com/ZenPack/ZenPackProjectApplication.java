447e5a3d7af8939293cb0c4a3da0545450b98d2c
vtguser <muruganeee005@gmai.com>
2022-10-14 20:48:17 +0530

    Final Commit

--- src/main/java/com/ZenPack/ZenPackProjectApplication.java
+++ src/main/java/com/ZenPack/ZenPackProjectApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
---
+++ src/main/java/com/ZenPack/utils/PostgresqlConfiguration.java
@@ -0,0 +1,75 @@
+package com.ZenPack.utils;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+
+@Configuration
+@Component
+///@Profile(value={"dev", "prod","uat","demo","premigration","local","ai"})
+public class PostgresqlConfiguration {
+
+
+	
+	/*
+	@NotNull
+	@Value("${db.username}")
+    private String username;
+	
+    @NotNull
+    @Value("${db.password}")
+    private String password;
+
+    @NotNull
+    @Value("${db.url}")
+    private String url;
+    
+    @Value("${db.driver}")
+    private String driver;   
+    
+    */
+    @Bean
+    DataSource loadDataSource() throws SQLException {   
+    	Map<String, String> data=DBUtils.zookeeperConfig();
+    	 String username=data.get("userName");
+    	 String password=data.get("password");
+         String url=data.get("url");
+         String driver="org.postgresql.Driver";
+    	
+    	//System.out.println("zookeeper data::"+data);
+         return DataSourceBuilder
+                 .create()
+                 .url(url)
+                 .username(username)
+                 .password(password)
+                 .driverClassName(driver)
+                 .build();
+    }
+   
+    @Bean
+    @Autowired
+    public JdbcTemplate loadJdbcTemplate(@Qualifier("loadDataSource")DataSource dataSource) {   	
+    	
+            return new JdbcTemplate(dataSource);
+        }
+   
+
+	@Bean
+	@Autowired
+	public NamedParameterJdbcTemplate loadNamedParameterJdbcTemplate(@Qualifier("loadDataSource")DataSource dataSource) {   	
+		
+	        return new NamedParameterJdbcTemplate(dataSource);
+	    }
+	}
+
